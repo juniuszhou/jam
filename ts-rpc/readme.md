d0925e9b5dddb4224dfec0965045e0100550cc5f
juniuszhou <junius.zhou@gmail.com>
2025-04-23 21:33:23 +0800

    remove out folder

--- ts-rpc/readme.md
+++
@@ -1,6 +0,0 @@
-# ts code test against polkavm
-
-## start the service in bun
-bun dev --host
-
-##
--- ts-rpc/src/hello.ts
+++
@@ -1,6 +0,0 @@
-function sayHello(name: string): string {
-    return `Hello, ${name}!`;
-}
-
-const greeting = sayHello("World");
-console.log(greeting);
